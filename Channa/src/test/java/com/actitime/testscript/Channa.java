9af887ac00c73afe76b754451b2dbd974c575c08
BanuPriya93 <129736978+BanuPriya93@users.noreply.github.com>
2023-06-07 12:24:47 +0530

    Create Channa.java

---
+++ Channa/src/test/java/com/actitime/testscript/Channa.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Channa {
+	@Test
+   public void testCreateCustomer() {
+	   Reporter.log("createCustomer",true);
+   }
+}
